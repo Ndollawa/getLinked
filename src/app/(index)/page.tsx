26d958bc189d246fc96668279b2105a013914c70
Ndubuisi O <foundictsolutions@gmail.com>
2023-09-25 21:01:30 +0100

    Update page.tsx

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
   useEffect(() => {
 
    async function animate() {
-      if (typeOf window !== "undefined") {
+      if (typeof window !== "undefined") {
         const ScrollReveal = (await import("scrollreveal")).default
        // sr().reveal(refToComponent.current)
   ScrollReveal({
